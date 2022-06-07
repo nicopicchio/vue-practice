6bfb6b2d5261efd1cba39bdfaf45e3a29354d8fa
Nico Picchio <nicolapicchio@gmail.com>
2022-06-07 16:23:37 +0100

    practice click events and conditional rendering

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const app = Vue.createApp()
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      title: 'The Silence Of The Lambs',
+      author: 'Thomas Harris',
+      releaseYear: 1991,
+      rating: 5
+    }
+  },
+  methods: {
+    increaseRating() {
+      if (this.rating === 10) return
+      else this.rating++
+    },
+    decreaseRating() {
+      if (this.rating === 1) return
+      else this.rating--
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks
+    }
+  }
+})
 
 app.mount('#app')
