2bab12d887f8c78471b848aba9117a0790445001
Nico Picchio <nicolapicchio@gmail.com>
2022-06-10 16:23:29 +0100

    assigning dynamic classes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
           author: 'Thomas Harris',
           releaseYear: 1991,
           rating: 5,
+          isRead: false,
           amazonLink: 'https://www.amazon.co.uk/Silence-Lambs-Hannibal-Lecter-Paperback/dp/B0161TKTG8/ref=sr_1_4?crid=2MRUBTEP880GP&keywords=the+silence+of+the+lambs&qid=1654871743&sprefix=the+silence+%2Caps%2C94&sr=8-4'
         },
         {
@@ -17,6 +18,7 @@ const app = Vue.createApp({
           author: 'Dan Brown',
           releaseYear: 2003,
           rating: 5,
+          isRead: false,
           amazonLink: 'https://www.amazon.co.uk/Vinci-Code-Exp-Dan-Brown/dp/052556585X/ref=sr_1_2?crid=19EOISKZPAIC3&keywords=the+davinci+code+book&qid=1654871825&sprefix=the+davin%2Caps%2C77&sr=8-2'
         },
         {
@@ -24,6 +26,7 @@ const app = Vue.createApp({
           author: 'Walter Isaacson',
           releaseYear: 2011,
           rating: 5,
+          isRead: false,
           amazonLink: 'https://www.amazon.co.uk/Steve-Jobs-Exclusive-Walter-Isaacson-ebook/dp/B005J3IEZQ/ref=sr_1_4?crid=2ZCT7WU45AT4K&keywords=steve+jobs&qid=1654871864&sprefix=steve+jobs%2Caps%2C85&sr=8-4'
         },
         {
@@ -31,6 +34,7 @@ const app = Vue.createApp({
           author: 'James Clear',
           releaseYear: 2018,
           rating: 5,
+          isRead: false,
           amazonLink: 'https://www.amazon.co.uk/Atomic-Habits-Proven-Build-Break/dp/1847941834/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1654871913&sr=8-1'
         },
         {
@@ -38,6 +42,7 @@ const app = Vue.createApp({
           author: 'Tom Clancy',
           releaseYear: 1984,
           rating: 5,
+          isRead: false,
           amazonLink: 'https://www.amazon.co.uk/Hunt-Red-October-Tom-Clancy/dp/0870212850/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1654871940&sr=8-2'
         },
         {
@@ -45,6 +50,7 @@ const app = Vue.createApp({
           author: 'Carol S. Dweck, Ph.D.',
           releaseYear: 2006,
           rating: 5,
+          isRead: false,
           amazonLink: 'https://www.amazon.co.uk/Mindset-Updated-Changing-Fulfil-Potential/dp/147213995X/ref=sr_1_1?crid=2U7SNLFNGJ77B&keywords=mindset+carol+dweck&qid=1654871967&sprefix=mindse%2Caps%2C85&sr=8-1'
         }
       ]
@@ -62,18 +68,8 @@ const app = Vue.createApp({
     toggleShowBooks() {
       this.showBooks = !this.showBooks
     },
-    handleMouseOver(e) {
-      console.log('Mouse Over: ', e, e.type)
-    },
-    handleMouseLeave(e) {
-      console.log('Mouse Left: ', e, e.type)
-    },
-    handleDoubleClick(e) {
-      console.log('Double click: ', e, e.type)
-    },
-    handleMouseMove(e) {
-      this.x = e.offsetX
-      this.y = e.offsetY
+    toggleIsRead(book) {
+      book.isRead = !book.isRead
     }
   }
 })
